2d6197555dde7d24374e810525cead3ed43ce90d
Adrian Cupcic <adrian.cupcic@gmail.com>
2023-04-14 10:40:43 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Adrian Cupcic on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
